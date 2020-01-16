b436ace8c6ec27f900e03d10870bdebcbc88a68c
Jeffrey Coe <jeffrey.coe@cerner.com>
2020-01-16 12:54:52 -0500

    Initial commit

---
+++ lib/aws-cloudformation-helper.rb
@@ -0,0 +1,3 @@
+module AWS::CloudFormation::Helper
+  GEM_VERSION = '0.1.0'
+end
---
+++ spec/aws/cloudformation/helper_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Aws::Cloudformation::Helper do
+  it "has a version number" do
+    expect(Aws::Cloudformation::Helper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
