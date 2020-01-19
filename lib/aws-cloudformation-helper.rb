20ba450b13317df629021f4f0834587bb807ee75
Jeffrey Coe <jeffrey.coe@cerner.com>
2020-01-19 13:32:26 -0500

    Added main initialization function

--- lib/aws-cloudformation-helper.rb
+++ lib/aws-cloudformation-helper.rb
@@ -1,3 +1,31 @@
-module AWS::CloudFormation::Helper
-  GEM_VERSION = '0.1.0'
+module AWS
+  module CloudFormation
+    module Helper
+      class << self
+        attr_accessor :logger
+        attr_accessor :log_level
+
+        attr_reader :context
+        attr_reader :event
+
+        def default_logger
+          @log_level = DEFAULT_LOG_LEVEL if @log_level == nil
+          Logger.new
+        end
+
+        def initialize(main_class, event, context)
+          # Ruby context object: https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
+          @context = context
+
+          # Initialize the event object
+          @event = AWS::CloudFormation::Helper::Event.new(event)
+          @event.execute(main_class.method(:create), main_class.method(:delete), main_class.method(:update))
+        end
+
+        DEFAULT_LOG_LEVEL = :info
+      end
+    end
+  end
 end
+
+AWS::CloudFormation::Helper.logger = AWS::CloudFormation::Helper.default_logger
