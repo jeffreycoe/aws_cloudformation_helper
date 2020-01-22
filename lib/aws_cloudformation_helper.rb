ce72b355483e860965d1d225bbab23002bcd9572
Jeffrey Coe <jeffrey.coe@cerner.com>
2020-01-21 23:58:54 -0500

    Creates event instance and logger objects

--- lib/aws_cloudformation_helper.rb
+++ lib/aws_cloudformation_helper.rb
@@ -9,33 +9,27 @@ module AWS
   module CloudFormation
     # Main module
     class Helper
-      class << self
-        attr_accessor :logger
-        attr_accessor :log_level
-        attr_accessor :data
+      attr_reader :event
+      attr_reader :context
 
-        attr_reader :context
-        attr_reader :event
+      def initialize(lambda_class, event, context)
+        # Ruby context object: https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
+        @context = context
 
-        def default_logger
-          @log_level = DEFAULT_LOG_LEVEL if @log_level.nil?
-          Logger.new
-        end
+        # Initialize event object
+        @event = Event.new(event, lambda_class.method(:create),
+                          lambda_class.method(:delete), lambda_class.method(:update))
+        Event.instance = @event
+      end
 
-        def initialize(main_class, event, context)
-          # Ruby context object: https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
-          @context = context
+      def self.logger
+        @logger ||= Logger.new
+        @logger
+      end
 
-          # Initialize the event object
-          @event = AWS::CloudFormation::Helper::Event.new(event)
-          logger.debug("Event: #{event.inspect}")
-          @event.execute(main_class.method(:create), main_class.method(:delete), main_class.method(:update))
-        end
-
-        DEFAULT_LOG_LEVEL = :info
+      def logger
+        self.class.logger
       end
     end
   end
 end
-
-AWS::CloudFormation::Helper.logger = AWS::CloudFormation::Helper.default_logger
