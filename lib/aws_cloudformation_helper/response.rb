445916b93faa011826540e3a6ec7cd23759e887e
Jeffrey Coe <jeffrey.coe@cerner.com>
2020-01-24 16:44:08 -0500

    Added retry logic if send response throws exception

--- lib/aws_cloudformation_helper/response.rb
+++ lib/aws_cloudformation_helper/response.rb
@@ -14,6 +14,10 @@ module AWS
         HTTP_MAX_RETRIES = 3
         SUCCESS_STATUS = 'SUCCESS'
 
+        def initialize
+          @http_retries = 1
+        end
+
         def failure(reason = '')
           status_code = send_response('PUT', Event.instance.response_url, provider_response(FAILURE_STATUS, reason))
           err_msg = "Failed to send failure message to CloudFormation pre-signed S3 URL. RC: #{status_code}"
@@ -25,7 +29,6 @@ module AWS
           http = Net::HTTP.new(uri.host, uri.port)
           http.use_ssl = (uri.scheme == 'https')
           request = http_request(method, uri, body)
-          retries ||= 1
 
           Helper.logger.debug("Sending #{method} request to URL #{response_url}...")
           response = http.request(request)
@@ -34,9 +37,10 @@ module AWS
           response.code.to_i
         rescue StandardError => e
           err_msg = 'Failed to send response to CloudFormation pre-signed S3 URL. '\
-          "(Attempt #{retries} of #{HTTP_MAX_RETRIES}) Error Details: #{e}"
+          "(Attempt #{@http_retries} of #{HTTP_MAX_RETRIES}) Error Details: #{e}"
           Helper.logger.error(err_msg)
-          retry if (retries += 1) <= HTTP_MAX_RETRIES
+          retry if (@http_retries += 1) <= HTTP_MAX_RETRIES
+          @http_retries = 1
           err_msg = 'Reached max retry attempts and failed to send message to CloudFormation pre-signed S3 URL.'
           Helper.logger.error(err_msg)
           raise e
